b546009a09d671190186b925593ee44dccfb523f
Aditya Gaurav <adityagaurav13a@gmail.com>
2026-04-01 19:56:19 +0000

    GFG : basis program 4

---
+++ input_in_python.py
@@ -0,0 +1,10 @@
+#You need to perform three separate tasks based on the given input:
+
+#String Input and Print: Read a text input as a string and print it as it is.
+#Integer Input and Add: Read an integer input n, add 10 to it, and print the result.
+#Float Input and Multiply: Read a floating-point number as input, multiply it by 10, and print the result.
+
+s = str(input("String:" ))
+n = int(input("integer:" ))
+f = float(input("float:" ))
+print(s + "\n" + str(n + 10) + "\n" + str(f * 10))     
